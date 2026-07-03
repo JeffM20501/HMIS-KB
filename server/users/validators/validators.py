8867d5c31d8c5e7ba6111ab90ee0dbe28a210dad
JeffM20501 <kimmuna440@gmail.com>
2026-07-03 15:37:36 +0300

    fixed circular imports issue on user model

--- server/users/validators/validators.py
+++ server/users/validators/validators.py
@@ -1,6 +1,8 @@
 
 from django.core.exceptions import ValidationError
-from users.models import User
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
 
 def validate_username(value):
     
