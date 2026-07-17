a7051da238629fa4c3793eb1d27b2013663bbb1e
JeffM20501 <kimmuna440@gmail.com>
2026-07-17 11:45:09 +0300

    chnages pagination maximum

--- server/app/settings.py
+++ server/app/settings.py
@@ -55,7 +55,7 @@ REST_FRAMEWORK={
         'rest_framework.authentication.SessionAuthentication' # auth for test
     ),
     "DEFAULT_PAGINATION_CLASS":"rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE":20,
+    "PAGE_SIZE":100,
 }
 
 SIMPLE_JWT={
