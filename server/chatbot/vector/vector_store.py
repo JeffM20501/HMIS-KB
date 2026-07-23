f0c509f09a9f34fc1a3c97e2291a4cb6089ca5f7
JeffM20501 <kimmuna440@gmail.com>
2026-07-23 23:47:48 +0300

    removed unwanted imports

--- server/chatbot/vector/vector_store.py
+++ server/chatbot/vector/vector_store.py
@@ -1,7 +1,6 @@
 # from langchain_community.vectorstores import Chroma
 from django.conf import settings
 from chatbot.embedding.chatbot_embedding import EmbeddingManager
-from langchain_chroma import Chroma
 from langchain_postgres import PGVector
 
 class VectorStoreManager:
@@ -11,17 +10,6 @@ class VectorStoreManager:
         self.embeddings = EmbeddingManager().get_embeddings()
         self.collection_name = collection_name
         self._init_pgvector()
-    
-    def _init_chroma(self):
-        """Use Chroma (local, file-based, good for development)."""
-        import os
-        persist_dir = os.path.join(settings.BASE_DIR, "chroma_db")
-        
-        self.vectorstore = Chroma(
-            collection_name=self.collection_name,
-            embedding_function=self.embeddings,
-            persist_directory=persist_dir,
-        )
     
     def _init_pgvector(self):
         """Use PostgreSQL + pgvector (production-ready)."""
