0181e10b3e52dd016f5d5cb7bc5313dd20368353
JeffM20501 <kimmuna440@gmail.com>
2026-07-24 10:10:58 +0300

    added cors managment for the frontend url

--- server/app/settings.py
+++ server/app/settings.py
@@ -113,6 +113,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS=[
+    'https://hmis-kb.pages.dev',
+    'http://localhost:5173',
+    
+]
+
+CORS_ALLOW_CREDENTIALS=True
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
