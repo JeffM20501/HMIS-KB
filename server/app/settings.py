b9b163a460f9cf3b3fc79385f9c798602d13d61c
JeffM20501 <kimmuna440@gmail.com>
2026-08-02 13:15:57 +0300

    use pymysql, remove auth_plugin

--- server/app/settings.py
+++ server/app/settings.py
@@ -172,7 +172,6 @@ if os.getenv('DB_HOST'):
             'OPTIONS': {
                 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
                 'charset': 'utf8mb4',
-                'auth_plugin': 'mysql_native_password',   # ← add this line
             }
         }
     }
