6105022c24483d739277083b6e7f33f4fcf2bc46
JeffM20501 <kimmuna440@gmail.com>
2026-08-05 13:50:47 +0300

    added api docs using swagger

--- server/app/urls.py
+++ server/app/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
+from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -40,4 +41,7 @@ urlpatterns = [
             path('token/verify', TokenVerifyView.as_view(), name='token_verify')
         ]))
     ])),
+    path('api/schema/', SpectacularAPIView.as_view(),name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'),name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
 ]
